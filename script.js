4133ca14795fd5671d947ffcd9e867350d0244e1
Sultan Mamud <167969089+sultan5dec@users.noreply.github.com>
2024-05-13 17:55:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+<script>
+        $(document).ready(function(){
+            $(".bars").click(function(){
+                $(".navbar-navigation").slideToggle(1550);
+            })
+        })
+    </script>
